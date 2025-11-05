df6bb2096469a5ef6a2425d2c1ecf1a4c41614c9
RstK <208619198+bowman025@users.noreply.github.com>
2025-11-05 13:09:59 +0100

    Change one word in return value

--- knights-travails.js
+++ knights-travails.js
@@ -11,7 +11,7 @@ const knightMoves = (start, end) => {
     while(queue.length > 0) {
         const [[x, y], num, steps] = queue.shift();
         if (x === end[0] && y === end[1]) {
-            return `You made it in ${num} steps! Here's your path:\n${steps}`;
+            return `You made it in ${num} moves! Here's your path:\n${steps}`;
         }
         for (const move of moves) {
             const current = [x + move[0], y + move[1]];
